5c2b6c781b3186971af3319e26051e309ea8a017
timdecool <tim.decool@gmail.com>
2025-02-08 12:30:52 +0100

    TD/Affichage_salaries_crees_4O4_exception_template

--- src/main/java/com/ipi/jva320/controller/SalarieController.java
+++ src/main/java/com/ipi/jva320/controller/SalarieController.java
@@ -4,10 +4,12 @@ import com.ipi.jva320.exception.SalarieException;
 import com.ipi.jva320.model.SalarieAideADomicile;
 import com.ipi.jva320.service.SalarieAideADomicileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 
@@ -20,8 +22,11 @@ public class SalarieController {
 
     @GetMapping("/{id}")
     public String getSalarie(@PathVariable Long id, ModelMap model) {
-        SalarieAideADomicile salarie = new SalarieAideADomicile("Jeannette Dupontelle", LocalDate.of(2021, 7, 1), LocalDate.now(), 0, 0, 10, 1, 0);
-        model.put("salarie", salarie);
+        SalarieAideADomicile salarie = salarieAideADomicileService.getSalarie(id);
+        model.put("sa", salarie);
+        if(salarie == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ce salarié n'existe pas.");
+        }
         return "detail_Salarie";
     }
 
