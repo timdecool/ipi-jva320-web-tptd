525a1f485140fdacdab42a15d1e481331f2a8beb
timdecool <tim.decool@gmail.com>
2025-02-08 12:27:01 +0100

    TD/Creation_salarie

--- src/main/java/com/ipi/jva320/controller/SalarieController.java
+++ src/main/java/com/ipi/jva320/controller/SalarieController.java
@@ -1,14 +1,13 @@
 package com.ipi.jva320.controller;
 
+import com.ipi.jva320.exception.SalarieException;
 import com.ipi.jva320.model.SalarieAideADomicile;
 import com.ipi.jva320.service.SalarieAideADomicileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -26,4 +25,15 @@ public class SalarieController {
         return "detail_Salarie";
     }
 
+    @GetMapping("/aide/new")
+    public String createSalarie(ModelMap model) {
+        model.put("salarie", new SalarieAideADomicile());
+        return "detail_Salarie";
+    }
+
+    @PostMapping("/save")
+    public String saveSalarie(@ModelAttribute("salarie") SalarieAideADomicile salarie) throws SalarieException {
+        salarieAideADomicileService.creerSalarieAideADomicile(salarie);
+        return "redirect:/salaries/" + salarie.getId();
+    }
 }
