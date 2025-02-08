8cf7233baa2d22f5fafeb52240fad520ac31e13f
timdecool <tim.decool@gmail.com>
2025-02-08 12:04:59 +0100

    TD/Nombre_salaries

--- src/main/java/com/ipi/jva320/controller/HomeController.java
+++ src/main/java/com/ipi/jva320/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.ipi.jva320.controller;
 
+import com.ipi.jva320.service.SalarieAideADomicileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private SalarieAideADomicileService salarieAideADomicileService;
+
+
     @GetMapping("/")
     public String home(final ModelMap model) {
+        Long numberOfEmployees = salarieAideADomicileService.countSalaries();
+        model.put("numberOfEmployees", numberOfEmployees);
         return "home";
     }
 }
